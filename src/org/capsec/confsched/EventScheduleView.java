9cc7071d1d0260517c0fa60cd4b7a7ff74242554
Joerg Bornschein <jb@capsec.org>
2011-04-03 01:01:14 +0200

    Minor widged experiments

--- src/org/capsec/confsched/EventScheduleView.java
+++ src/org/capsec/confsched/EventScheduleView.java
@@ -8,6 +8,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
@@ -28,13 +29,13 @@ public class EventScheduleView extends View {
 
     protected void onDraw(Canvas canvas) {
     	Paint paint = new Paint();
-    	paint.setColor(0xEEff8888);
-    	paint.setStyle(Paint.Style.STROKE);
+    	paint.setColor(0x888888ff);
+    	paint.setStyle(Paint.Style.FILL_AND_STROKE);
     	paint.setAntiAlias(true);
     	
-    	RectF rect = new RectF(10, 10, 100, 100);
+    	RectF rect = new RectF(50, 10, 400, 200);
+    	canvas.drawRoundRect(rect, 7, 7, paint);
     	
-    	canvas.drawRect(rect, paint);
     }
     
 }
